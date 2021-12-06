135adef4615b2ecbac14cd936db296968ab071c4
Robbie08 <rortiz8838@sdsu.edu>
2021-12-06 14:56:18 -0800

    Fixed applications display on reload, user object is now accessible from job applications component

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {CookiesProvider} from 'react-cookie';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>,
-  document.getElementById('root')
+    <CookiesProvider>
+      <React.StrictMode>
+        <App/>
+      </React.StrictMode>
+    </CookiesProvider>,
+    
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
